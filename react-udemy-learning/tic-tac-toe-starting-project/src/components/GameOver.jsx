0b253056ed5ff8885587dc001302d55ff817f690
Akhilesh Deshmukh <deshmukhakhilesh9@gmail.com>
2026-09-11 07:00:34 +0530

    feat(tic-tac-toe): add game-over and turn log UI

---
+++ react-udemy-learning/tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+export default function GameOver({ winner, onRestart }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over!</h2>
+      {winner && <p>{winner} wins!</p>}
+      {!winner && <p>It's a Draw!</p>}
+      <p>
+        <button onClick={onRestart}>Play Again!</button>
+      </p>
+    </div>
+  );
+}
