e143e986f2b519c30b325256b928e088cb61472c
Akhilesh Deshmukh <deshmukhakhilesh9@gmail.com>
2026-09-12 21:50:23 +0530

    feat(investment-calculator): add investment UI and form

---
+++ react-udemy-learning/investment-calculator-project/src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!isInputValid}
+      <Results userInput={userInput} />
+    </main>
+  );
+}
+
+export default App;
